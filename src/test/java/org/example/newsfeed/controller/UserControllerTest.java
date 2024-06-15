e0d4ee60f4dd286e00b0396712aca42b6660999d
byeongik11 <mice2008@naver.com>
2024-06-15 13:17:46 +0900

    test commit

---
+++ src/test/java/org/example/newsfeed/controller/UserControllerTest.java
@@ -0,0 +1,36 @@
+package org.example.newsfeed.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserControllerTest {
+
+    @Test
+    void signup() {
+    }
+
+    @Test
+    void withdrawUser() {
+    }
+
+    @Test
+    void getUser() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void updatePassword() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void logout() {
+    }
+}
